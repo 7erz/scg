a0eb1110fd8fdfeb51a1d420e7a227188464c2fa
Lee, Juseok <leesncat@naver.com>
2021-11-02 10:36:10 +0900

    fifth.5 commit
    add remark

--- eclipse-workspace/scg project/src/ossproject/scg.java
+++ eclipse-workspace/scg project/src/ossproject/scg.java
@@ -3,7 +3,14 @@ import javax.swing.*;
 import java.awt.font.*;
 import java.awt.*;
 import java.awt.event.*;
-
+//9월 30일 시작
+//자바를 알지 못해 난잡한 코드들로 처리하느라 정리가 불가능한 상황에 왔음
+// 그래서 독학과 GUI 수업을 통해 First란 창을 새로 띄워서 10월 말 다시 코드를 작성했음
+// 일단 시간이 너무 늦은 관계로 밈에 관련된 간단한 선택형 게임을 만들 예정
+// 첫번쨰는 간단한 틀과 화면 전환
+// 두번쨰는 퀴즈들의 내용들과 처리
+// 세번쨰로는 음악과 사운드 추가
+// 마지막으로 기말 전까지 세세한것을 다듬을 것
 public class scg extends JFrame implements ActionListener{
 Container c = getContentPane();
 Font Bold = new Font("궁서체",Font.BOLD,60);
@@ -36,7 +43,8 @@ JPanel btnpnl = new JPanel();
 		warnheadlbl.setFont(Bold);
 		warnheadlbl.setForeground(Color.RED);
 		warnheadlbl.setHorizontalAlignment(JLabel.CENTER);
-		warnsublbl.setText("<html>이것은 ???인 내용을 포함하고 있습니다.<br/>특정 성향에 따라 불쾌할수 있으므로 주의 바립니다.</html>");
+		//warnsublbl.setText("<html>이것은 ???인 내용을 포함하고 있습니다.<br/>특정 성향에 따라 불쾌할수 있으므로 주의 바립니다.</html>");
+		warnsublbl.setText("<html>최근에 시작해서 아무것도 없습니다.<br/>코드 보고 저급하다고 해도 저는 모릅니다.</html>");
 		warnsublbl.setForeground(Color.white);
 		warnsublbl.setFont(Plane);
 		warnsublbl.setHorizontalAlignment(JLabel.CENTER);
@@ -75,7 +83,7 @@ JPanel btnpnl = new JPanel();
 			setVisible(false);
 		}
 	}
-	
+	//10월 28일 시작
 	class First extends JFrame{
 		//상단창 프레임 제거 (예정)
 		Frame frame = new Frame();
@@ -149,7 +157,7 @@ JPanel btnpnl = new JPanel();
 		}
 		
 		class StartHandler implements ActionListener{
-			//시작버튼
+			//시작버튼, 화면 전환 필요함
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource()==fbtn[0]) {
 					for(int i = 0; i < 3; i++) {
@@ -160,7 +168,7 @@ JPanel btnpnl = new JPanel();
 			}
 		}
 		class CantcancelHandler implements ActionListener{
-			//종료 안되는 종료 버튼
+			//종료 안되는 종료 버튼 ( 패널티 부여 )
 			public void actionPerformed(ActionEvent e) {
 				if(e.getSource()==fbtn[1]) {
 					JOptionPane.showMessageDialog(null, nametxt.getText()+"님 종료는 못하십니다.");
